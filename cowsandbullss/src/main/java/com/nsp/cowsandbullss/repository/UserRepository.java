88add244e08a0dc93665d64e691146e985767391
LingarajuR <lingarajurnie@gmail.com>
2024-12-19 05:04:39 +0530

    Delete cowsandbullss/src directory

--- cowsandbullss/src/main/java/com/nsp/cowsandbullss/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nsp.cowsandbullss.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nsp.cowsandbullss.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
